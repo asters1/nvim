4eb4d5b9d7818ec32d6d026decbbd97a114d89fd
Asters1 <gj744559140@163.com>
2024-07-18 16:26:47 +0800

    windowns_vim

---
+++ init.lua
@@ -0,0 +1,11 @@
+-- HOME = os.getenv("HOME")
+
+HOME = "C:\\Users\\ASTERS\\AppData\\Local\\nvim"
+gh = "https://github.wisteria.cf/"
+-- print(HOME)
+--基础
+require("util.func")
+require("config.options")
+require("config.keymaps")
+require("config.auto")
+require("config.plugins")
---
+++ lua/config/plugins.lua
@@ -0,0 +1,49 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- print(lazypath)
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    gh .. "folke/lazy.nvim",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)                                             
+vim.keymap.set("n", "<leader><leader>", ":Lazy<CR>", { noremap = true })
+
+require("lazy").setup({
+  --  主题
+  -- require("config.plugins.colorscheme").background,
+  require("config.plugins.colorscheme").statusline2,
+  -- 注释
+  require("config.plugins.comment"),
+  -- 目录树
+  require("config.plugins.nvim-tree"),
+  --标题栏 
+  require("config.plugins.tabtitle"),
+  -- 语法高亮 
+  require("config.plugins.treesitter"),
+  --缩进线 (可能需要先安装语法高亮)
+  require("config.plugins.indent"),
+
+  ----------------LSP----------------
+  -- require("config.plugins.lsp"),
+  -- require("config.plugins.lspconfig").config,
+  --   -- --LSP与自动补全                                                               
+    -- require("config.plugins.lsp"), 
+    require("config.plugins.mason"),
+    require("config.plugins.cmp"),
+    require("config.plugins.lsp_signature"),    
+    require("config.plugins.autoformat") 
+  --   --LSP与自动补全并且自动格式化       
+  --   -- require("config.plugins.mason"),
+  --   -- require("config.plugins.autocomplete").config,
+  --   -- require("config.plugins.lspconfig").config,
+
+
+
+
+
+}, {                                                                            })
